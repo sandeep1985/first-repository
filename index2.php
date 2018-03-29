4ddaf10e8c6a4cd6ae5de3bb20b84b32cd402e51
sandeep <sandeep_verma1985@yahoo.com>
2018-03-29 10:07:42 -0400

    testing

--- index2.php
+++ index2.php
@@ -1,2 +1,39 @@
-this is another file
-test
+<div style="width:100%;background-color:#F46D2D;text-align: center;">
+    <div>
+        <br />
+        <img alt="Measure Roi"  src="https://accuratewebsolution.com/wp-content/uploads/2018/03/measure-roi.jpg"/> 
+    </div>
+    <br /> 
+    
+    <div style="width:70%;margin:0 auto;padding:2%"> 
+    <h2 style="color:#fff;font-size:50px">MEASURE ROI</h2>
+    <p style="font-size:25px;color:#E2F7C0;text-align: left;">
+        &bull;  Comprehensive Campaign Analytics <br />
+        &bull;  Google Adwords Integration <br />
+        &bull;  Exportable Graphs And Repors <br />
+        &bull;  Eliminate Waste<br />
+        &bull;  Invest in Successful Tactics<br />
+        &bull;  Content Roi Tracking 
+    </p>
+    </div>
+    
+</div>
+
+<div style="width:100%;background-color:#19B1C2;text-align: center;height: 470px;">
+    <div>
+        <br /><br />
+        <img alt="Drive Sales"  src="https://accuratewebsolution.com/wp-content/uploads/2018/03/drive-sales.jpg"/> 
+    </div>
+    <br /> 
+    
+    <div style="width:70%;margin:0 auto;padding:2%;padding-bottom: 3%;"> 
+    <h2 style="color:#fff;font-size:50px">DRIVE SALES</h2>
+    <p style="font-size:25px;color:#E2F7C0;text-align: left;">
+        &bull;  Lead Scoring To Alert Hot Leads <br />
+        &bull;  Email Automation For Nurturing <br />
+        &bull;  Behaviour Tracking For Leads &nbsp;&nbsp;Insight <br />
+        &bull;  Sales Notifications <br /><br />
+    </p>
+    </div>
+    
+</div>
