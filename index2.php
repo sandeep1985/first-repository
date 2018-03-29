e488901823cd422b48801f6563434e8cfab4823f
sandeep <sandeep_verma1985@yahoo.com>
2018-03-29 13:33:49 -0400

    changes

--- index2.php
+++ index2.php
@@ -19,21 +19,168 @@
     
 </div>
 
-<div style="width:100%;background-color:#19B1C2;text-align: center;height: 470px;">
+<div style="width:100%;background-color:#19B1C2;text-align: center;">
     <div>
         <br /><br />
         <img alt="Drive Sales"  src="https://accuratewebsolution.com/wp-content/uploads/2018/03/drive-sales.jpg"/> 
     </div>
     <br /> 
     
-    <div style="width:70%;margin:0 auto;padding:2%;padding-bottom: 3%;"> 
+    <div style="width:70%;margin:0 auto;padding:2%;padding-bottom: 4%"> 
     <h2 style="color:#fff;font-size:50px">DRIVE SALES</h2>
     <p style="font-size:25px;color:#E2F7C0;text-align: left;">
         &bull;  Lead Scoring To Alert Hot Leads <br />
         &bull;  Email Automation For Nurturing <br />
         &bull;  Behaviour Tracking For Leads &nbsp;&nbsp;Insight <br />
-        &bull;  Sales Notifications <br /><br />
-    </p>
+        &bull;  Sales Notifications <br /></p>
     </div>
     
 </div>
+
+<div style="float:left"><img alt="Email Automation"  src="https://accuratewebsolution.com/wp-content/uploads/2018/03/email-automation.jpg"/> </div>
+<div style="float:left;margin-left:2%;margin-top:2%">
+    <h3 style="color:#E26C1F">Email Automation</h3>
+    <p style="color:#B3B3B4;font-size:25px">
+    Send emails with<br /> triggers and build <br>personal relationship
+    </p>
+</div>
+
+
+<div style="float:left"><img alt="Rebrandable"  src="https://accuratewebsolution.com/wp-content/uploads/2018/03/email-automation.jpg"/> </div>
+<div style="float:left;margin-left:2%;margin-top:2%">
+    <h3 style="color:#E26C1F">Rebrandable</h3>
+    <p style="color:#B3B3B4;font-size:25px">
+    Rebrand the entire platform<br /> and reinforce your branding <br/>with clients every day
+    </p>
+</div>
+
+<div style="float:left"><img alt="Single Sign-on"  src="https://accuratewebsolution.com/wp-content/uploads/2018/03/single.jpg"/> </div>
+<div style="float:left;margin-left:2%;margin-top:2%">
+    <h3 style="color:#E26C1F">Single Sign-On</h3>
+    <p style="color:#B3B3B4;font-size:25px">
+    Manage all your clients<br /> from a single instance
+    </p>
+</div>
+
+<div style="float:left"><img alt="Dynamic Form"  src="https://accuratewebsolution.com/wp-content/uploads/2018/03/dynamic-form.jpg"/> </div>
+<div style="float:left;margin-left:2%;margin-top:2%">
+    <h3 style="color:#E26C1F">Dynamic Forms</h3>
+    <p style="color:#B3B3B4;font-size:25px">
+    Capture more leads<br /> with form designed<br />to convert
+    </p>
+</div>
+
+<div style="float:left"><img alt="Dynamic Form"  src="https://accuratewebsolution.com/wp-content/uploads/2018/03/dynamic-content.jpg"/> </div>
+<div style="float:left;margin-left:2%;margin-top:2%">
+    <h3 style="color:#E26C1F">Dynamic Web Content</h3>
+    <p style="color:#B3B3B4;font-size:25px">
+    Close more sales with<br /> content that grows with<br />your leads
+    </p>
+</div>
+
+<div style="float:left"><img alt="Dynamic Form"  src="https://accuratewebsolution.com/wp-content/uploads/2018/03/lead-score.jpg"/> </div>
+<div style="float:left;margin-left:2%;margin-top:2%">
+    <h3 style="color:#E26C1F">Lead Scoring</h3>
+    <p style="color:#B3B3B4;font-size:25px">
+    Prioritize your pipeline <br/>and reach out to <br /> sales-ready leads
+    </p>
+</div>
+
+<div style="float:left"><img alt="Dynamic Form"  src="https://accuratewebsolution.com/wp-content/uploads/2018/03/sales-notofication.jpg"/> </div>
+<div style="float:left;margin-left:2%;margin-top:2%">
+    <h3 style="color:#E26C1F">Sales Notifications</h3>
+    <p style="color:#B3B3B4;font-size:25px">
+    Email or text sales team<br/>when a lead indicates<br /> they are ready to buy
+    </p>
+</div>
+
+<div style="float:left"><img alt="crm"  src="https://accuratewebsolution.com/wp-content/uploads/2018/03/crm-integration.jpg"/> </div>
+<div style="float:left;margin-left:2%;margin-top:2%">
+    <h3 style="color:#E26C1F">CRM Integration</h3>
+    <p style="color:#B3B3B4;font-size:25px">
+    Immediately plug your<br/>leads into your sales<br /> team's system
+    </p>
+</div>
+
+
+
+
+<div style="float:left"><img alt="Social Media"  src="https://accuratewebsolution.com/wp-content/uploads/2018/03/social-media.jpg"/> </div>
+<div style="float:left;margin-left:2%;margin-top:2%">
+    <h3 style="color:#E26C1F">Social Media Management</h3>
+    <p style="color:#B3B3B4;font-size:25px">
+    Publish, listen and track<br/>campaigns with fully integrated<br /> social media tools.
+    </p>
+</div>
+
+<div style="float:left"><img alt="Gmail"  src="https://accuratewebsolution.com/wp-content/uploads/2018/03/gmail-email.jpg"/> </div>
+<div style="float:left;margin-left:2%;margin-top:2%">
+    <h3 style="color:#E26C1F">Gmail/Email Syncingt</h3>
+    <p style="color:#B3B3B4;font-size:25px">
+    Emails sent and received<br/>from IMAP clients show<br /> up right in leads' histories
+    </p>
+</div>
+
+
+<div style="float:left"><img alt="AB"  src="https://accuratewebsolution.com/wp-content/uploads/2018/03/a-b.jpg"/> </div>
+<div style="float:left;margin-left:2%;margin-top:2%">
+    <h3 style="color:#E26C1F">A/B Testing</h3>
+    <p style="color:#B3B3B4;font-size:25px">
+    Test emails head-to-head<br/>to see exactly what works<br /> the best
+    </p>
+</div>
+
+<div style="float:left"><img alt="Behavior Tracking"  src="https://accuratewebsolution.com/wp-content/uploads/2018/03/behaviour.jpg"/> </div>
+<div style="float:left;margin-left:2%;margin-top:2%">
+    <h3 style="color:#E26C1F">Behavior Tracking</h3>
+    <p style="color:#B3B3B4;font-size:25px">
+    Understand your leads<br/>to create one-on-one<br /> communication
+    </p>
+</div>
+
+<div style="float:left"><img alt="Sales Analytics"  src="https://accuratewebsolution.com/wp-content/uploads/2018/03/sales-analytics.jpg"/> </div>
+<div style="float:left;margin-left:2%;margin-top:2%">
+    <h3 style="color:#E26C1F">Sales Analytics</h3>
+    <p style="color:#B3B3B4;font-size:25px">
+    Enable your sales team<br/>with key insights about<br /> each lead
+    </p>
+</div>
+
+<div style="float:left"><img alt="Campaign Optimization"  src="https://accuratewebsolution.com/wp-content/uploads/2018/03/campaign-optimization.jpg"/> </div>
+<div style="float:left;margin-left:2%;margin-top:2%">
+    <h3 style="color:#E26C1F">Campaign Optimization</h3>
+    <p style="color:#B3B3B4;font-size:25px">
+    Eliminate waste and<br/>identify opportunities
+    </p>
+</div>
+
+<div style="float:left;width:30%"><img alt="Would You"  src="https://accuratewebsolution.com/wp-content/uploads/2018/03/would-you.jpg"/> </div>
+<div style="float:left;width:70%"><img alt="Shout"  src="https://accuratewebsolution.com/wp-content/uploads/2018/03/shout.jpg"/></div>
+
+
+<div>
+    <img alt="More Revenue"  src="https://accuratewebsolution.com/wp-content/uploads/2018/03/more-revenue.jpg"/> 
+    <h2 style="color:#E26C1F;font-size:35px">MORE REVENUE FOR YOU</h2>
+    <p style="font-size:28px;color:#B6B6B6;text-align: left;">
+        Marketing automation allows you to forge rich relationship, and by developing your customers, your business will be discovered with more leads driving sales and proving ROI.
+    </p>
+</div>
+
+<div>
+    <img alt="451"  src="https://accuratewebsolution.com/wp-content/uploads/2018/03/451.jpg"/> <br>
+<br>    <h2 style="color:#E26C1F;font-size:35px">THE BEST COMMUNICATION TOOLS</h2>
+    <p style="font-size:28px;color:#B6B6B6;text-align: left;">
+        Marketing automation is a leap forward in digital marketing tools. For the first time, your business will have the resorces to connect with each customer on a one-on-one basic.
+    </p>
+</div>
+
+<div>
+    <img alt="3"  src="https://accuratewebsolution.com/wp-content/uploads/2018/03/3.jpg"/> 
+    <div style="margin-top:55px">
+        <h2 style="color:#E26C1F;font-size:35px">ANALYTICS FOR OPTIMIZATION</h2>
+        <p style="font-size:28px;color:#B6B6B6;text-align: left;">
+            Marketing automation identifies successful tactics and poor performences. This allows you to eliminate waste and improve of ROI.
+        </p>
+    </div>
+</div>
+
